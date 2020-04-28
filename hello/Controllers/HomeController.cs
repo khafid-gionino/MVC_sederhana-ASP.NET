f0cb1d7e73b0b4126112c2d20e5c1a2c2a4922e0
khafid-gionino <achmadkhafids75@gmail.com>
2020-04-28 13:41:40 +0700

    commit

---
+++ hello/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using hello.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hello.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Person> _person = new List<Person>()
+            {
+                new Person {Name = "ACH.KHAFID SALIM", Npm = "06.2017.1.06762" }
+            };
+            return View(_person);
+        }
+    }
+}
